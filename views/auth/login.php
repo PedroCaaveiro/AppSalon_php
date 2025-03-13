5d13304f66f4c3d13e59992b7a5d8c4f133d05ae
PedroCaaveiro <acaaveir@yahoo.es>
2025-03-13 08:33:48 +0100

    cambios

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,23 @@
-<h1>login</h1>
+<h1 class="nombre-pagina">Login</h1>
+<p class="descripcion-pagina">Iniciar sesion</p>
+
+<form action="/" class="formulario" method="POST">
+
+<div class="campo">
+    <label for="email">Email</label>
+    <input type="email" id="email" placeholder="Tu Email" name="email">
+</div>
+
+<div class="campo">
+    <label for="password">Password</label>
+    <input type="password" id="password" placeholder="Tu Password" name="password">
+</div>
+
+<input type="submit" class="boton" value="Iniciar Sesión">
+
+</form>
+
+<div class="acciones">
+    <a href="/crear-cuenta">Crear Cuenta</a>
+    <a href="/olvide">¿Olvidaste tu Password?</a>
+</div>
