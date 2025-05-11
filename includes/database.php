9c566a8e7f50f3003904c9136bc04f387b0948d6
PedroCaaveiro <acaaveir@yahoo.es>
2025-05-11 11:39:23 +0200

    cambios

--- includes/database.php
+++ includes/database.php
@@ -1,11 +1,11 @@
 <?php
 
 require_once 'app.php';
-//$db = mysqli_connect('localhost', 'root', 'root', 'appsalon_mvc');
-$db_produccion = mysqli_connect('DB_HOST', 'DB_USER', 'DB_PASS', 'DB_NAME');
+$db = mysqli_connect('localhost', 'root', 'root', 'appsalon_mvc');
+//$db = mysqli_connect('DB_HOST', 'DB_USER', 'DB_PASS', 'DB_NAME');
 
 
-if (!$db_produccion) {
+if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
     echo "errno de depuración: " . mysqli_connect_errno();
     echo "error de depuración: " . mysqli_connect_error();
